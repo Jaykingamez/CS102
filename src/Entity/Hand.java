52d0b37eeebb0ad2feb9c6b191db91c63c5976e9
Jaykingamez <jaykinchan@gmail.com>
2024-03-02 15:08:36 +0800

    Style: Update Hand.java to Java Coding Conventions

--- src/Entity/Hand.java
+++ src/Entity/Hand.java
@@ -1,13 +1,13 @@
+/* Hand.java - John K. Estell - 8 May 2003
+ * last modified: 23 Febraury 2004
+ * Implementation of a abstract hand of playing cards.
+ * Uses the Card class.  Requires subclass for specifying
+ * the specifics of what constitutes the evaluation of a hand
+ * for the game being implemented.
+ */
 package Entity;
-// Hand.java - John K. Estell - 8 May 2003
-// last modified: 23 Febraury 2004
-// Implementation of a abstract hand of playing cards.  
-// Uses the Card class.  Requires subclass for specifying
-// the specifics of what constitutes the evaluation of a hand 
-// for the game being implemented.
 
 import java.util.*;
-
 
 /**
  * Represents the basic functionality of a hand of cards.
@@ -19,17 +19,15 @@ import java.util.*;
  */
 public abstract class Hand implements Comparable<Hand> {
     
-   private ArrayList<Card> hand = new ArrayList<Card>(); 
-
+   private final ArrayList<Card> hand = new ArrayList<>();
 
   /**
    * Adds a card to this hand.
    * @param card card to be added to the current hand.
    */
-   public void addCard( Card card ) {
-      hand.add( card );
+   public void addCard(Card card) {
+      hand.add(card);
    }
-
 
   /**
    * Obtains the card stored at the specified location in the hand.  Does not
@@ -38,25 +36,20 @@ public abstract class Hand implements Comparable<Hand> {
    * @return the card of interest, or the null reference if the index is out of
    * bounds.
    */
-   public Card getCard( int index ) {
-      return hand.get( index );
+   public Card getCard(int index) {
+      return hand.get(index);
    }
-  
-  
+
   /**
    * Removes the specified card from the current hand.
    * @param card the card to be removed.
    * @return the card removed from the hand, or null if the card
    * was not present in the hand.
    */
-   public Card removeCard( Card card ) {
-      int index = hand.indexOf( card );
-      if ( index < 0 )
-         return null;
-      else
-         return hand.remove( index );
+   public Card removeCard(Card card) {
+      int index = hand.indexOf(card);
+      return (index < 0) ? null : hand.remove(index);
    }
-
 
   /**
    * Removes the card at the specified index from the hand.
@@ -64,10 +57,9 @@ public abstract class Hand implements Comparable<Hand> {
    * @return the card removed from the hand, or the null reference if
    * the index is out of bounds.
    */
-   public Card removeCard( int index ) {
-      return hand.remove( index );
+   public Card removeCard(int index) {
+      return hand.remove(index);
    }
-
 
   /**
    * Removes all the cards from the hand, leaving an empty hand.
@@ -75,7 +67,6 @@ public abstract class Hand implements Comparable<Hand> {
    public void discardHand() {
       hand.clear();
    }
-
 
   /**
    * The number of cards held in the hand.
@@ -85,15 +76,13 @@ public abstract class Hand implements Comparable<Hand> {
       return hand.size();
    }
 
-
   /**
    * Sorts the card in the hand.
    * Sort is performed according to the order specified in the {@link Card} class.
    */
    public void sort() {
-      Collections.sort( hand );
+      Collections.sort(hand);
    }
-
 
   /**
    * Checks to see if the hand is empty.
@@ -103,27 +92,24 @@ public abstract class Hand implements Comparable<Hand> {
       return hand.isEmpty();
    }
 
-
   /**
    * Determines whether or not the hand contains the specified card.
    * @param card the card being searched for in the hand.
    * @return <code>true</code> if the card is present in the hand.
    */
-   public boolean containsCard( Card card ) {
+   public boolean containsCard(Card card) {
       return false;
    }
-
 
   /**
    * Searches for the first instance of the specified card in the hand.
    * @param card card being searched for.
    * @return position index of card if found, or <code>-1</code> if not found.
    */
-   public int findCard( Card card ) {
-      return hand.indexOf( card );
+   public int findCard(Card card) {
+      return hand.indexOf(card);
    }
-  
-  
+
   /**
    *  Compares two hands.  
    *  @param otherHandObject the hand being compared.
@@ -131,20 +117,17 @@ public abstract class Hand implements Comparable<Hand> {
    *  the same, or > 0 if this hand is greater then the other hand.
    */
    @Override
-   public int compareTo( Hand otherHandObject ) {
-      Hand otherHand = otherHandObject;
-      return evaluateHand() - otherHand.evaluateHand();
+   public int compareTo(Hand otherHandObject) {
+      return evaluateHand() - otherHandObject.evaluateHand();
    }
-   
-   
+
   /**
    *  Evaluates the hand.  Must be defined in the subclass that implements the hand
    *  for the game being written by the client programmer.
    *  @return an integer corresponding to the rating of the hand.
    */
    public abstract int evaluateHand();
-   
-   
+
    /**
     * Returns a description of the hand.
     * @return a list of cards held in the hand.
@@ -152,20 +135,19 @@ public abstract class Hand implements Comparable<Hand> {
     public String toString() {
         return hand.toString();
     }
-    
-    
+
    /**
     * Replaces the specified card with another card.  Only the first
     * instance of the targeted card is replaced.  No action occurs if
     * the targeted card is not present in the hand.
     * @return <code>true</code> if the replacement occurs.
     */
-    public boolean replaceCard( Card oldCard, Card replacementCard ) {
-        int location = findCard( oldCard );
-        if ( location < 0 )
-           return false;
-        hand.set( location, replacementCard );
+    public boolean replaceCard(Card oldCard, Card replacementCard) {
+        int location = findCard(oldCard);
+        if (location < 0){
+            return false;
+        }
+        hand.set(location, replacementCard);
         return true;
     }
-
 }
