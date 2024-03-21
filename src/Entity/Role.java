e551399c14e36ed5a317085913f1fef197043932
SJL1504 <161947563+SJL1504@users.noreply.github.com>
2024-03-21 10:22:51 +0800

    Create Role.java

---
+++ src/Entity/Role.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Role {
+    private String roleName;
+
+    public Role(){
+        // Create an instance of the Random class
+        Random random = new Random();
+
+        // Generate a random number between 0 and 99
+        int randomNumber = random.nextInt(100); // Generates numbers in the range [0, 100)
+
+        // Define a probability threshold for getting 0 --- probability of getting 0 is 60%
+        int zeroProbabilityThreshold = 30;
+
+        // Check if the random number falls below the probability threshold
+        if (randomNumber < zeroProbabilityThreshold) {
+            // Set the generated number to 0
+            randomNumber = 0;
+        }
+
+        if(randomNumber == 1){
+            roleName = "King";
+            System.out.println("ROLE: King");
+            System.out.println("description: At the begining of each turn flip over a card from the deck and if you guess correct you win the game!");
+        }
+        if(randomNumber == 2){
+            roleName = "Jester";
+            System.out.println("ROLE: Jester");
+            System.out.println("description: At the begining of each turn draw a card from the deck and if you all of your cards add upp to below 10 you win the game!!");
+        }
+        if(randomNumber == 0){
+            roleName = "Citizen";
+            System.out.println("ROLE: Citizen");
+        }
+
+        
+    }
+
+    public String getRoleName(){
+        return this.roleName;
+    }
+
+    
+
+}
