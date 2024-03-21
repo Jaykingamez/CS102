dccb07c031ec9a7ad4bf438246775541beb915c4
dyler turd <158392101+tyler-durd@users.noreply.github.com>
2024-03-22 00:30:03 +0800

    comments on player.java

--- player.java
+++ player.java
@@ -2,8 +2,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-
-public class player {
+//Denxi 3.22.24
+public class Player { 
     private String playerName;
     private int value; // Player's current balance
 
@@ -28,3 +28,10 @@ public class player {
 }
 
 
+/*
+1. Changed class name to Player with a capital 'P' for consistency with the rest of the methods.
+2. Since Player is an object that can be instantiated, "value" in the methods could be changed to "this.value" just for extra clarity. Note that it's not actually wrong to not put it though because if there's
+only one of "value" within the method, it automatically takes it as "this.value". Itll be clearer that it's an instance variable and could makethings less confusing as new methods are added.
+3. in line 10, the player method should not be void because it doesn't return anything. It's only to instantiate a new player, so just "public Player(int initialValue)..." will be sufficient! :) 
+
+*/
