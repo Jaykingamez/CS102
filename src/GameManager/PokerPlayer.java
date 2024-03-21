e83f5333f6ee057f41ea4a897c8d123e7a93f574
Kkskeng <kkseah.2023@scis.smu.edu.sg>
2024-03-21 12:41:41 +0800

    progress + more comments

--- src/GameManager/PokerPlayer.java
+++ src/GameManager/PokerPlayer.java
@@ -4,12 +4,12 @@ public class PokerPlayer {
     String name = "Yeow";
     int currentChips = 200;
     Hand hand;
-    boolean allIn = false;
-    boolean lastStandAcknowledge = false;
-    boolean firstTurn = false;
-    boolean finishedTurn = false;
-    boolean finishedRound = false;
-    int currentBet = 5;
+    boolean allIn = false; //player in 'all in' state, skip their turns and ignore call/raise resets, include them during showdown
+    boolean lastStandAcknowledge = false;   //player acknowledges they are forced to all in during initial bet, skip all turns after
+    boolean playedTurn = false;//player has called or checked this turn, resets when raise happens
+    boolean finishedRound = false;//player is finished with making inputs via folding or all in
+    boolean folded = false;//player in 'folded' state, skip their turns and ignore call/raise resets, ignore them during showdown
+    int currentBet = 0;//amount they have bet, added into pot after each phase
 
     public PokerPlayer(String name, int currentChips) {
         this.name = name;
