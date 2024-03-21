e842db6a8a366cab4211d770fd22e5d3889167fd
dyler turd <158392101+tyler-durd@users.noreply.github.com>
2024-03-22 00:53:28 +0800

    Update pot.java
    - Changed class to uppercase Pot 
    - Comments w.r.t naming of player- does "playermaybe" refer to an instance of a player? If so you can just use Player 
    - im falling asleep

--- pot.java
+++ pot.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class pot {
+//Denxi 3.21.24
+
+public class Pot {
     private int totalAmount;
     private List<playermaybe> contributors;
 
-    public pot() {
+    public Pot() {
         totalAmount = 0;
         contributors = new ArrayList<>();
     }
@@ -37,3 +39,11 @@ public class pot {
         return contributors;
     }
 }
+
+/*
+Comments from miya
+1. playermaybe is kind of ambiguous, but if we do use it it should be in lower camel case e.g playerMaybe, frogValue
+2. I'm assuming the pot will be divided evenly with rounding to an integer?
+3. Please communicate with jialin on how to integrate yalls pot files since u both made them omg sry im sleepy 
+
+*/
