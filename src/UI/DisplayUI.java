6deac9f3050d9ef90c6fccc56b0f189375db1ed1
Jaykingamez <jaykinchan@gmail.com>
2024-03-21 12:26:10 +0800

    WIP

--- src/UI/DisplayUI.java
+++ src/UI/DisplayUI.java
@@ -27,17 +27,21 @@ public class DisplayUI{
     // Pass in player and game information
     public DisplayUI(){
         ImageIcon icon = new ImageIcon("images/2c.gif");
+
+        // Label contains player's image and info
         JLabel label = new JLabel();
         String playerString = String.format("Player info %s%nMoney: %d%nBet: %d ",
          "Yeow Leong", 500, 100);
         JTextArea playerInfoText = new JTextArea(playerString);
-        //label.setText();
+        
+        // Transform player's image
         ImageIcon yeowLeong = new ImageIcon("images/yllee.jpg");
         Image image = yeowLeong.getImage(); // transform it 
         Image newimg = image.getScaledInstance(200, 200,  java.awt.Image.SCALE_SMOOTH); // scale it smoothly  
         yeowLeong = new ImageIcon(newimg);  // assign to a new ImageIcon instance
         label.setIcon(yeowLeong);
 
+        // Contain's player's role and win condition
         String winCondition = String.format("Win Conditon:%n Role: %s%n Description: %s",
         "jester", "Just die lmao");
         JTextArea winConditionText = new JTextArea(winCondition);
@@ -53,45 +57,52 @@ public class DisplayUI{
         JLabel label6 = new JLabel(icon);
         JLabel label7 = new JLabel(icon);
 
-        
+        // Contains player name, picture, money and betted money
         playerInfo.setBackground(Color.red);
         playerInfo.setBounds(0, 0, 250, 250);
 
-        
+        // Lists player role and their win condition
         winConditionPanel.setBackground(Color.blue);
         winConditionPanel.setBounds(750, 0, 250, 250);
 
-
+        // Shows all community cards
         tablePanel.setBackground(Color.green);
         tablePanel.setBounds(250, 0, 500, 250);
 
-
+        // Shows all cards in player's hand
         handPanel.setBackground(Color.black);
         handPanel.setBounds(250, 250, 500, 250);
 
-
+        // Shows all passive action player can take
+        // FOLD, CHECK
         passiveActionPanel.setBackground(Color.pink);
         passiveActionPanel.setBounds(0, 250, 250, 250);
 
+        // FOLD button 
         JButton fold = new JButton();
         fold.setPreferredSize(new Dimension(150, 50));
         fold.setText("FOLD");
 
+        // CHECK button
         JButton check = new JButton();
         check.setPreferredSize(new Dimension(150, 50));
         check.setText("CHECK");
 
+        // Add FOLD and CHECK to panel
         passiveActionPanel.add(fold);
         passiveActionPanel.add(check);
 
-
+        // Shows all active action player can take 
+        // RAISE, CALL
         activeActionPanel.setBackground(Color.magenta);
         activeActionPanel.setBounds(750, 250, 250, 250);
 
+        // RAISE button
         JButton raise = new JButton();
         raise.setPreferredSize(new Dimension(150, 50));
         raise.setText("RAISE");
 
+        // CALL button
         JButton call = new JButton();
         call.setPreferredSize(new Dimension(150, 50));
         call.setText("CALL");
@@ -99,12 +110,13 @@ public class DisplayUI{
         activeActionPanel.add(raise);
         activeActionPanel.add(call);
         
+        // Frame that contains all UI elements
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
         frame.setSize(1000, 500);
         frame.setVisible(true);
-
+         
         playerInfo.add(playerInfoText);
         playerInfo.add(label);
 
--- src/UI/MainMenuUI.java
+++ src/UI/MainMenuUI.java
@@ -8,58 +8,83 @@ import java.awt.*;        // Using AWT layouts
 import javax.swing.*;     // Using Swing components and containers
 
 public class MainMenuUI {
+    // Contains Yeow Leong's face
     JPanel leftPanel = new JPanel();
+    // Contains Lay Foo's face
     JPanel rightPanel = new JPanel();
+    // Organises Centre Panel
     GridLayout gridLayout = new GridLayout(4, 1);
-    JPanel bluePanel = new JPanel();
+    // Contains Manu and all the buttons
+    JPanel centrePanel = new JPanel();
+    // Start button starts the game
     JButton start = new JButton();
+    // Profile creates user profile
     JButton profile = new JButton();
+    // Exit quits the game
     JButton exit = new JButton();
+    // The frame holding the game
     JFrame frame = new JFrame();
+    // Contains title screen
+    JLabel titleLabel = new JLabel();
 
     // Pass in player class so that UI can process player information
     public MainMenuUI(){
         // Setup left panel (Yeow Leong)
         leftPanel.setBounds(0, 0, 250, 500);
+
+        // resize image to fill the entire panel
         ImageIcon icon = new ImageIcon("images/yllee.jpg");
         Image image = icon.getImage(); // transform it 
         Image newimg = image.getScaledInstance(250, 500,  java.awt.Image.SCALE_SMOOTH); // scale it smoothly  
         icon = new ImageIcon(newimg);  // assign to a new ImageIcon instance
+
+        // Pass image to label
         JLabel label = new JLabel(icon);
         leftPanel.add(label);
         
         // Setup right panel (Lay Foo)
         rightPanel.setBounds(750, 0, 250, 500);
+
+        // resize image to fill the entire panel
         icon = new ImageIcon("images/lfthiang.png");
         image = icon.getImage(); // transform it 
         newimg = image.getScaledInstance(250, 500,  java.awt.Image.SCALE_SMOOTH); // scale it smoothly  
         icon = new ImageIcon(newimg);  // assign to a new ImageIcon instance
+
+        // Pass image to label
         label = new JLabel(icon);
         rightPanel.add(label);
         
         //setup Centre Panel (Title and Buttons)
-        bluePanel.setBackground(Color.white);
-        bluePanel.setBounds(250, 0, 500, 500);
-        bluePanel.setLayout(gridLayout);
+        centrePanel.setBackground(Color.white);
+        centrePanel.setBounds(250, 0, 500, 500);
+        centrePanel.setLayout(gridLayout);
 
+        // Set up Li Yuchen and centre screen
         icon = new ImageIcon("images/yuchenli.png");
-        label = new JLabel(icon);
-        label.setText("Texas Holdem: Revenge of the Yeow Leong");
-        bluePanel.add(label);
+        titleLabel.setIcon(icon);
+        titleLabel.setText("Texas Holdem: Revenge of the Yeow Leong");
+        centrePanel.add(titleLabel);
 
         // Start game
         start.setPreferredSize(new Dimension(150, 50));
         start.setText("START");
         start.addActionListener(e -> {
+            // kill the screen and go to the main game
+            // need to pass it something that stores game information
             frame.dispose();
             new DisplayUI();
         });
-        bluePanel.add(start);
+        centrePanel.add(start);
 
         // Player Account
         profile.setPreferredSize(new Dimension(150, 50));
         profile.setText("PROFILE");
-        bluePanel.add(profile);
+        profile.addActionListener(e -> {
+            String name = JOptionPane.showInputDialog("What is your name?");
+            titleLabel.setText("Hello " + name + " It's me, your best friend Dr Li Yuchen :)");
+        });
+        centrePanel.add(profile);
 
         // Quit game
         exit.setPreferredSize(new Dimension(150, 50));
@@ -67,22 +92,15 @@ public class MainMenuUI {
         exit.addActionListener(e -> {
             frame.dispose();
         });
-        bluePanel.add(exit);
+        centrePanel.add(exit);
 
+        // set up frame containing all UI elements
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
         frame.setSize(1000, 500);
         frame.setVisible(true);
-        frame.add(bluePanel);
+        frame.add(centrePanel);
         frame.add(leftPanel);
         frame.add(rightPanel);
     }
-    public static void main(String[] args) {
-        
-        
-       
-
-    }
-    
-    
 }
