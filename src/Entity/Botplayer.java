8edae7a78d0a1c10408a1432645448997f845b66
SJL1504 <161947563+SJL1504@users.noreply.github.com>
2024-03-21 10:21:23 +0800

    Create Botplayer.java

---
+++ src/Entity/Botplayer.java
@@ -0,0 +1,22 @@
+public class BotPlayer extends Player{
+    
+    public BotPlayer(String name, Hand pHand, Role pRole){
+        super(name, pHand, pRole);
+    }
+
+    public Hand getpHand(){
+        return super.getpHand();
+    }
+
+    public String getName(){
+        return super.getName();
+    }
+
+    public boolean getActive(){
+        return super.getActive();
+    }
+
+    public Role getPRole(){
+        return super.getPRole();
+    }
+}
