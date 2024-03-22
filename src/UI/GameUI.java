3c69b119e30be28f5284cca537c1eec44a65dfa1
Jaykingamez <jaykinchan@gmail.com>
2024-03-23 00:01:38 +0800

    rename DisplayUI to GameUI

--- src/UI/GameUI.java
+++ src/UI/GameUI.java
@@ -8,31 +8,41 @@ import java.awt.*;        // Using AWT layouts
 
 import javax.swing.*;     // Using Swing components and containers
 
-public class DisplayUI{
-    // Contains player name, picture, money and betted money
-    JPanel playerInfo = new JPanel();
-    // Lists player role and their win condition
-    JPanel winConditionPanel = new JPanel();
-    // Shows all community cards
-    JPanel tablePanel = new JPanel();
-    // Shows all cards in player's hand
-    JPanel handPanel = new JPanel();
-    // Shows all passive action player can take
-    // FOLD, CHECK
-    JPanel passiveActionPanel = new JPanel();
-    // Shows all active action player can take 
-    // RAISE, CALL
-    JPanel activeActionPanel = new JPanel();
+import Controller.GameController;
+import Entity.Card;
+import Entity.Player;
+
+public class GameUI{
+    // Shows player info
+    JTextArea playerInfoText = new JTextArea();
+    
 
     // Pass in player and game information
-    public DisplayUI(){
+    public GameUI(GameController gameController){
         ImageIcon icon = new ImageIcon("images/2c.gif");
+
+        // Contains player name, picture, money and betted money
+        JPanel playerInfo = new JPanel();
+        // Lists player role and their win condition
+        JPanel winConditionPanel = new JPanel();
+        // Shows all community cards
+        JPanel tablePanel = new JPanel();
+        // Shows all cards in player's hand
+        JPanel handPanel = new JPanel();
+        // Shows all passive action player can take
+        // FOLD, CHECK
+        JPanel passiveActionPanel = new JPanel();
+        // Shows all active action player can take 
+        // RAISE, CALL
+        JPanel activeActionPanel = new JPanel();
 
         // Label contains player's image and info
         JLabel label = new JLabel();
         String playerString = String.format("Player info %s%nMoney: %d%nBet: %d ",
          "Yeow Leong", 500, 100);
         JTextArea playerInfoText = new JTextArea(playerString);
+
+        updatePlayerInfo(gameController.getPlayer());
         
         // Transform player's image
         ImageIcon yeowLeong = new ImageIcon("images/yllee.jpg");
@@ -138,113 +148,29 @@ public class DisplayUI{
         frame.add(activeActionPanel);
     }
 
-    public static void main(String[] args) {
-        ImageIcon icon = new ImageIcon("images/2c.gif");
-        JLabel label = new JLabel();
+    public void updatePlayerInfo(Player player){
         String playerString = String.format("Player info %s%nMoney: %d%nBet: %d ",
-         "Yeow Leong", 500, 100);
-        JTextArea playerInfoText = new JTextArea(playerString);
-        //label.setText();
-        ImageIcon yeowLeong = new ImageIcon("images/yllee.jpg");
-        Image image = yeowLeong.getImage(); // transform it 
-        Image newimg = image.getScaledInstance(200, 200,  java.awt.Image.SCALE_SMOOTH); // scale it smoothly  
-        yeowLeong = new ImageIcon(newimg);  // assign to a new ImageIcon instance
-        label.setIcon(yeowLeong);
+        player.getName(), player.getCurrentChips(), player.getCurrentBet());
+        playerInfoText.setText(playerString);
+    }
 
+    public void updateTableCards(List<Card> cards){
+        tablePanel.removeAll();
+        for (Card card: cards){
+            JLabel label = new JLabel(card.getCardImage());
+            tablePanel.add(label);
+        }
+    }
+
+    public void updateRole(Player player){
         String winCondition = String.format("Win Conditon:%n Role: %s%n Description: %s",
-        "jester", "Just die lmao");
+        player.getPRole().getRoleName(), player.getPRole().getRoleDescription());
         JTextArea winConditionText = new JTextArea(winCondition);
 
-        // Create multiple JLabels with the same image
-        // table cards
-        JLabel label2 = new JLabel(icon);
-        JLabel label3 = new JLabel(icon);
-        JLabel label4 = new JLabel(icon);
-        JLabel label5 = new JLabel(icon);
+    }
 
-        // hand cards
-        JLabel label6 = new JLabel(icon);
-        JLabel label7 = new JLabel(icon);
-
-        JPanel playerInfo = new JPanel();
-        playerInfo.setBackground(Color.red);
-        playerInfo.setBounds(0, 0, 250, 250);
-
-        JPanel winConditionPanel = new JPanel();
-        winConditionPanel.setBackground(Color.blue);
-        winConditionPanel.setBounds(750, 0, 250, 250);
-
-        JPanel tablePanel = new JPanel();
-        tablePanel.setBackground(Color.green);
-        tablePanel.setBounds(250, 0, 500, 250);
-
-        JPanel handPanel = new JPanel();
-        handPanel.setBackground(Color.black);
-        handPanel.setBounds(250, 250, 500, 250);
-
-        JPanel passiveActionPanel = new JPanel();
-        passiveActionPanel.setBackground(Color.pink);
-        passiveActionPanel.setBounds(0, 250, 250, 250);
-
-        JButton fold = new JButton();
-        fold.setPreferredSize(new Dimension(150, 50));
-        fold.setText("FOLD");
-
+    public void updatePassiveAction(boolean showFold, boolean showCheck){
         JButton check = new JButton();
-        check.setPreferredSize(new Dimension(150, 50));
-        check.setText("CHECK");
-
-        passiveActionPanel.add(fold);
-        passiveActionPanel.add(check);
-
-        JPanel activeActionPanel = new JPanel();
-        activeActionPanel.setBackground(Color.magenta);
-        activeActionPanel.setBounds(750, 250, 250, 250);
-
-        JButton raise = new JButton();
-        raise.setPreferredSize(new Dimension(150, 50));
-        raise.setText("RAISE");
-
-        JButton call = new JButton();
-        call.setPreferredSize(new Dimension(150, 50));
-        call.setText("CALL");
-
-        activeActionPanel.add(raise);
-        activeActionPanel.add(call);
-        
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(null);
-        frame.setSize(1000, 500);
-        frame.setVisible(true);
-
-        playerInfo.add(playerInfoText);
-        playerInfo.add(label);
-
-        tablePanel.add(label2);
-        tablePanel.add(label3);
-        tablePanel.add(label4);
-        tablePanel.add(label5);
-
-        winConditionPanel.add(winConditionText);
-
-        handPanel.add(label6);
-        handPanel.add(label7);
-
-        frame.add(playerInfo);
-        frame.add(winConditionPanel);
-        frame.add(tablePanel);
-        frame.add(handPanel);
-        frame.add(passiveActionPanel);
-        frame.add(activeActionPanel);
-
-
-        // // Run GUI codes in Event-Dispatching thread for thread-safety
-        // SwingUtilities.invokeLater(new Runnable() {
-        //     @Override
-        //     public void run() {
-        //     new DisplayUI();  // Let the constructor do the job
-        //     }
-        // });
+        check.setVisible(show);
     }
 }
