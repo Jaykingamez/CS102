b295894bcf7a842a52d13c3f8b34bf2bc395a8bf
denxifenn <144514992+denxifenn@users.noreply.github.com>
2024-03-23 00:49:14 +0800

    Update player.java

--- player.java
+++ player.java
@@ -1,30 +1,44 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
-public class player {
+public class Player {
     private String playerName;
     private int value; // Player's current balance
+    private int currentChips = 200;
+    private int currentBet;
 
-    public void Player(int initialValue) {
+    public Player(int initialValue) {
         this.value = initialValue;
     }
 
     // Method to deduct money from player's balance
     public void deductBalance(int amount) {
-        value -= amount;
+        this.value -= amount;
     }
 
     // Method to add money to player's balance
     public void addBalance(int amount) {
-        value += amount;
+        this.value += amount;
     }
 
     // Getter method for player's balance
     public int getValue() {
-        return value;
+        return this.value;
     }
+
+    public String getName(){
+        return this.playerName;
+    }
+
+    public void setCurrentChips(int chips) {
+        this.currentChips = chips;
+    }
+
+    public int getCurrentChips() {
+        return this.currentChips;
+    }
+
+    public int getCurrentBet() {
+        return currentBet;
+    }
+
 }
 
 
