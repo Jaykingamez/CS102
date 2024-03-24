7f41e91998d2d9ec2f4594f9ac014cadcef87b25
SJL1504 <161947563+SJL1504@users.noreply.github.com>
2024-03-24 19:48:52 +0800

    Update Pot.java

--- src/Entity/Pot.java
+++ src/Entity/Pot.java
@@ -1,4 +1,4 @@
-package Entity;
+// Jia lin Last edit 24/03/2024
 
 import java.util.*;
 
@@ -12,15 +12,28 @@ public class Pot {
     public Pot(List<Player> players){
         // if we not doing big bind and small bind then everyone at the start of the game can place $10
         //into the pot
+        // change start bet accordingly
         this.playerBets = new HashMap<>();
         for(int i = 0; i < players.size(); i++){
-            playerBets.put(players.get(i), 10);
+            playerBets.put(players.get(i), 10); // this start bet change accordingly
         }
 
         this.totalpot = 0;
 
         this.betToContinue = 10;
 
+    }
+
+    public Map<Player, Integer> getPlayersBets(){
+        return this.playerBets;
+    }
+
+    public int getBetToContinue(){
+        return this.betToContinue;
+    }
+
+    public int getTotalPot(){
+        return this.totalpot;
     }
 
     public void endTurnPot(){
@@ -34,7 +47,7 @@ public class Pot {
         //code for going through the HashMap and deteucting the players
         // turnBets(Values in the HashMap) from their Bank
         for(Player p : playerBets.keySet()){
-            p.setMoney(0);
+            p.detuctAmount(playerBets.get(p));
         }
 
         //all player bets becomes 0
@@ -53,35 +66,10 @@ public class Pot {
         playerBets.put(p, betToContinue);
     }
 
-    //used for calling in the action class
+    //used for calling
     public void updateBetToContinue(Player p){
         playerBets.put(p, betToContinue);
     }
 
-    public int getBetToContinue() {
-        return this.betToContinue;
-    }
-
-    public Map<Player,Integer> getPlayerBets() {
-        return this.playerBets;
-    }
-
-    public void setTotalPot(int newTotal) {
-        this.totalpot = newTotal;
-    }
-
-     // Method to distribute the pot to the winners
-     public void distributePot(Player... winners) {
-        int amountPerWinner = totalpot / winners.length;
-        for (Player winner : winners) {
-            winner.setMoney(winner.getMoney() + amountPerWinner);
-        }
-        totalpot = 0; // Reset pot
-        betToContinue = 10;
-
-        for(Player p : playerBets.keySet()){
-            playerBets.put(p, 10);
-        }
-    }
     
 }
