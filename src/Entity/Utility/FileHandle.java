b71826e5fbe67d6e47a48272dc8132681b18527b
dyler turd <158392101+tyler-durd@users.noreply.github.com>
2024-03-24 23:50:09 +0800

    Update and rename src/Entity/FileHandle.java to src/Entity/Utility/FileHandle.java

--- src/Entity/Utility/FileHandle.java
+++ src/Entity/Utility/FileHandle.java
@@ -7,8 +7,6 @@ import java.io.FileWriter;
 import java.lang.Exception.*;
 import java.util.Arrays;
 
-  // Import the File class
-
 public class FileHandle {
 
     
@@ -22,19 +20,5 @@ public class FileHandle {
       System.out.println("Failed to delete the file.");
     } 
   } 
-
-  // public void clearFile(String fileName) {
-
-
-  //   new File(fileName).();
-
-  // }
-
-  public void delAll(){
-
-    Arrays.asList(new File("data").listFiles()).stream().forEach(File::delete);
-    BufferedWriter bw = new BufferedWriter(new FileWriter("data"+decisionList.get(i)+"_"+i+".txt"));
-  }
-
 
 }
