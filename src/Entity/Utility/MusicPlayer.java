9bf85746603cce7ad39a1cc0563a2b4db6911ff1
Jaykingamez <jaykinchan@gmail.com>
2024-03-26 18:27:24 +0800

    Add MusicPlayer.java

---
+++ src/Entity/Utility/MusicPlayer.java
@@ -0,0 +1,34 @@
+package Entity.Utility;
+import java.io.*;
+import javazoom.jl.player.*;
+
+public class MusicPlayer extends Thread {
+
+    private String fileLocation;
+    private boolean loop;
+    private Player player;
+
+    public MusicPlayer (String fileLocation, boolean loop) {
+        this.fileLocation = fileLocation;
+        this.loop = loop;
+    }
+
+    public void run() {
+
+        try {
+            do {
+                FileInputStream buff = new FileInputStream(fileLocation);
+                player = new Player(buff);
+                player.play();
+            } while (loop);
+        } catch (Exception e) {
+            e.printStackTrace();;
+        }
+    }
+
+    public void close(){
+        loop = false;
+        player.close();
+        this.interrupt();
+    }
+}
