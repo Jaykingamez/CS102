2082ded12e0e6e2fd63f1d26b151d6e26214d364
Jaykingamez <jaykinchan@gmail.com>
2024-03-27 16:45:09 +0800

    delete all useless class files

--- src/GameManager/MainMenu.java
+++ src/GameManager/MainMenu.java
@@ -11,16 +11,14 @@ public class MainMenu {
 
     
     public static void main(String[] args) {
-        System.out.println("Poker Game Main Menu");
-        System.out.println("Input: ");
-        System.out.println("0 to Start Game");
-        System.out.println("1 to Create Profile");
-        System.out.println("2 to Exit Game");
-        Scanner scanner = new Scanner(System.in);
-        String input = "";
-
         while (!exit) {
-            input = scanner.nextLine();
+            System.out.println("Poker Game Main Menu");
+            System.out.println("Input: ");
+            System.out.println("0 to Start Game");
+            System.out.println("1 to Create Profile");
+            System.out.println("2 to Exit Game");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
             if (!input.matches("[012]")){
                 System.out.println("Invalid input. Please enter 0, 1, or 2.");
                 continue;
@@ -49,13 +47,14 @@ public class MainMenu {
         System.out.println("What is your name?");
         Scanner scanner = new Scanner(System.in);
         String input = "";
-        while (!input.isEmpty()){
+        while (input.isEmpty() || playerName.equals("Player")){
             input = scanner.nextLine();
             if (input.isEmpty()){
                 System.out.println("Please type in an appropriate name");
+                continue;
             }
+            playerName = input;
         }
-        playerName = input;
         PlayerNameSaver.saveName(input);
     }
 }
