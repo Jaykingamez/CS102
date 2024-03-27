7cbb5014c0321ac03c2cfcd52130b02b47e9571a
Jaykingamez <jaykinchan@gmail.com>
2024-03-27 17:23:42 +0800

    update gameStarted to initialize players

--- src/GameManager/GameManager.java
+++ src/GameManager/GameManager.java
@@ -5,25 +5,28 @@ Si Jie
 Last Updated: 26/3/2024
 */
 package GameManager;
+import java.util.*;
+
 import Entity.Game.*;
 
 public class GameManager {
     private PokerGame game;
 
-    public void gameStarted(String playerName) {
-        Player player = new Player(playerName, new Hand(), null);
-        Player bot1 = new Player("Player 2", new Hand(), null);
-        Player bot2 = new Player("Player 3", new Hand(), null);
-        Player bot3 = new Player("Player 4", new Hand(), null);
-        Player[] players = new Player[]{player, bot1, bot2, bot3};
+    public void gameStarted(String playerName, int humanPlayers, int botPlayers) {
+        List<Player> players = new ArrayList<>();
+        for (int i = 0; i < humanPlayers; i++){
+            players.add(new Player(playerName + i, new Hand(), null));
+        }
 
-        game = new PokerGame(players);
+        for (int i = 0; i < botPlayers; i++){
+            players.add(new BotPlayer("Bot " + i, new Hand()));
+        }
+
+        game = new PokerGame(players.toArray(new Player[0]));
         game.startGame();
     }
 
     public Pot getPot(){
         return game.getPot();
     }
-
-
 }
